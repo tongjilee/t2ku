0f309534deef8d4376d0f7b20a305487c6ddb465
P.S.V.R_linode <pmq2001@gmail.com>
2011-10-19 09:18:35 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+T2ku::Application.config.secret_token = '1a0773ab54a2973b5249b11ce1797324950ebdc983e66d9ae51d8d2d95e81bd66f97ccfbe5340106f7202d3af13e6b73adc158b04c0823038d0e3e00e4bca083'
